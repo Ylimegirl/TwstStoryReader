c30f44969732371e43788b96792e9de5d4436d01
Ylimegirl <87622158+Ylimegirl@users.noreply.github.com>
2021-09-13 16:52:00 -0700

    Adjusted output
    Tweaked format of parsed files and reworked the in-console Python text
    Updated README.md

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ To use, place the .JSON files you want parsed inside an "inputs" directory, and 
 `python reader.py`
 
 **If a file is *not* properly formatted as a JSON file, this code will not work!**
+
+## Known issues
+- Doesn't account for pathing or choices
+- May miss certain types of flavor text
+- Output could use some retooling in formatting
--- reader.py
+++ reader.py
@@ -1,11 +1,14 @@
 import re
 import json
 import os
+
 if not os.path.exists("inputs"):
 	os.mkdir("inputs")
 if not os.path.exists("outputs"):
 	os.mkdir("outputs")
 files = os.listdir("inputs")
+
+print("Parsing files...")
 
 for item in files:
 	#grab original text
@@ -18,17 +21,19 @@ for item in files:
 	if os.path.exists(newname):#deletes _parsed file if it exists
 		os.remove(newname)
 	new_file = open(newname, "a", encoding="utf-8")
+	new_file.write("Label\tContent\n---------------------------------------------")
 	for group in dict:
 		for x in dict[group]:
 			for y, z in x.items(): #i don't know if there's a way to do this without three goddamn levels of nesting no. twst's file formatting sucks
 				if y == "serif" and "text" in z.keys() and "speaker" in z.keys():
-					new_file.write(z["speaker"]["text"] + "\t" + re.sub("\n", "\n\t", z["text"]) + "\n")
+					new_file.write("\n" + z["speaker"]["text"] + "\t" + re.sub("\n", "\n\t", z["text"]))
 				elif y == "title" and "textWhite" in z.keys() and "textGold" in z.keys():
-					new_file.write("TITLE\t" + z["textWhite"] + "\n\t(" + z["textGold"] + ")\n")
+					new_file.write("\nTITLE\t" + z["textWhite"] + "\n\t(" + z["textGold"] + ")")
 				elif y == "place" and "jp" in z.keys() and "en" in z.keys():
-					new_file.write("PLACE\t" + z["jp"] + "\n\t" + z["en"] + "\n")
+					new_file.write("\nPLACE\t" + z["jp"] + "\n\t" + z["en"])
 				elif y == "balloon" and "text" in z.keys() and "speaker" in z.keys():
-					new_file.write(z["speaker"] + "\t" + re.sub("\n", "\n\t", z["text"]) + "\n")
+					new_file.write("\n" + z["speaker"] + "\t" + re.sub("\n", "\n\t", z["text"]))
 	new_file.close()
-	print("Parsed " + item + ".")
-print("\nFinished parsing all files.")
+	print("> Parsed " + item)
+
+print("Finished parsing all files.")
