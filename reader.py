c19e5eb2366278451ab392d92b2d940868777ec6
Ylimegirl <87622158+Ylimegirl@users.noreply.github.com>
2021-09-14 13:47:47 -0700

    v0.2.0
    Added commented-out code for pretty json files
    Can handle voice line jsons now
    Handles a few more cases in story jsons, and will let the user know if a certain case isn't handled yet

--- reader.py
+++ reader.py
@@ -8,6 +8,14 @@ if not os.path.exists("outputs"):
 	os.mkdir("outputs")
 files = os.listdir("inputs")
 
+def prettyJSON(file_name): # code to make prettified output of json files
+	if not os.path.exists("pretty"):
+		os.mkdir("pretty")
+	pretty = open("pretty/" + file_name[0:file_name.find(".")] + "_pretty.json", "w")
+	pretty.write(json.dumps(json.loads(parsed), indent = 4))
+	pretty.close() #inb4 no i do not know how to make these not output into unicode escaped characters, sorry!!
+
+
 print("Parsing files...")
 
 for item in files:
@@ -16,6 +24,9 @@ for item in files:
 	parsed = ref.read()
 	ref.close()
 	
+	#prettified output (for debugging/reference)
+	#prettyJSON(item)
+	
 	dict = json.loads(parsed)#converts json to dict format
 	newname = "outputs/" + item[0:item.find(".")] + "_parsed.txt"
 	if os.path.exists(newname):#deletes _parsed file if it exists
@@ -23,16 +34,44 @@ for item in files:
 	new_file = open(newname, "a", encoding="utf-8")
 	new_file.write("Label\tContent\n---------------------------------------------")
 	for group in dict:
-		for x in dict[group]:
-			for y, z in x.items(): #i don't know if there's a way to do this without three goddamn levels of nesting no. twst's file formatting sucks
-				if y == "serif" and "text" in z.keys() and "speaker" in z.keys():
-					new_file.write("\n" + z["speaker"]["text"] + "\t" + re.sub("\n", "\n\t", z["text"]))
-				elif y == "title" and "textWhite" in z.keys() and "textGold" in z.keys():
-					new_file.write("\nTITLE\t" + z["textWhite"] + "\n\t(" + z["textGold"] + ")")
-				elif y == "place" and "jp" in z.keys() and "en" in z.keys():
-					new_file.write("\nPLACE\t" + z["jp"] + "\n\t" + z["en"])
-				elif y == "balloon" and "text" in z.keys() and "speaker" in z.keys():
-					new_file.write("\n" + z["speaker"] + "\t" + re.sub("\n", "\n\t", z["text"]))
+		if group.startswith("group"):
+			for x in dict[group]:
+				for y, z in x.items(): #i don't know if there's a way to do this without three goddamn levels of nesting no. twst's file formatting sucks
+					if y == "serif" and "text" in z.keys() and "speaker" in z.keys():# handles main dialogue (serifs)
+						if "visible" in z["speaker"].keys():
+							if z["speaker"]["visible"] == False:# handles serifs w/o a speaker
+								new_file.write("\n[N/A]\t" + re.sub("\n", "\n\t", z["text"]))
+						else:
+							new_file.write("\n" + z["speaker"]["text"] + "\t" + re.sub("\n", "\n\t", z["text"]))
+					elif y == "serif" and not "text" in z.keys(): # skips serifs without text
+						pass
+					elif y == "title" and "textWhite" in z.keys() and "textGold" in z.keys(): # handles titles
+						if "personal" in z.keys(): # handles titles for personal stories
+							new_file.write("\nTITLE\t" + z["personal"]["rarity"] + " " + z["textGold"] + "\n\t" + z["personal"]["anotherName"] + ": " + z["textWhite"])
+						else: # handles other titles
+							new_file.write("\nTITLE\t" + z["textWhite"] + "\n\t" + z["textGold"])
+					elif y == "title" and not "textWhite" in z.keys(): # skips titles without text
+						pass
+					elif y == "text" and "text" in z.keys(): # handles generic "text" content
+						new_file.write("\nTEXT\t" + re.sub("\n", "\n\t", z["text"]))
+					elif y == "place" and "jp" in z.keys() and "en" in z.keys(): # handles place titles
+						new_file.write("\nPLACE\t" + z["jp"] + "\n\t" + z["en"])
+					elif y == "place" and not "jpn" in z.keys(): # skips places without text
+						pass
+					elif y == "telop" and "text" in z.keys(): #handles "telops"
+						new_file.write("\nTELOP\t" + z["text"])
+					elif y == "telop" and not "text" in z.keys(): # skips telops without text
+						pass
+					elif y == "balloon" and "text" in z.keys() and "speaker" in z.keys(): # handles speech balloons (in rhythmics and battle cutscenes)
+						new_file.write("\n" + z["speaker"] + "\t" + re.sub("\n", "\n\t", z["text"]))
+					elif y == "balloon" and not "text" in z.keys(): # skips balloons without text
+						pass
+					elif y == "live2d" or y == "moveCamera" or y == "systemUI" or y == "run" or y == "bgm" or y == "advBgOperator" or y == "touch" or y == "wait" or y == "zoomCamera" or y == "se" or y == "curtain" or y == "spine" or y == "spineCharacter" or y == "sd" or y == "spfx" or y == "shakeCamera": # skips a bunch of boring animation/visual logicistical code
+						pass
+					else: # for debugging mostly and to catch types i missed
+						new_file.write("\n" + y + "\t(no code to handle this type of object yet, sorry! --Ylime)")
+		elif group.startswith("voice"):
+			new_file.write("\n" + group + "\t" + dict[group]["serif"])#voice line jasons are so simple bless
 	new_file.close()
 	print("> Parsed " + item)
 
